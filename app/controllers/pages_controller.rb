3027de38f6d8fdfc848fdb9313161a68cb07df2c
Javier Cuadrado <mantecajc@gmail.com>
2023-04-03 22:18:55 +0200

    feat: upgrade ruby to 3.0.3 & gemfile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,10 @@
 class PagesController < ApplicationController
   def home
     content = PagesContent.first
-    @title = content.home_title
-    @description = content.home_description
-    @primary_photo = content.home_primary_photo
-    @secondary_photo = content.home_secondary_photo
+    @title = content.try(:home_title)
+    @description = content.try(:home_description)
+    @primary_photo = content.try(:home_primary_photo)
+    @secondary_photo = content.try(:home_secondary_photo)
   end
 
   def additional_info
