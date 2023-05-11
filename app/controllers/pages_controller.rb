4e53631a4eb437bcc7335d5184ace615d7398b90
Javier Cuadrado <mantecajc@gmail.com>
2023-05-11 21:59:37 +0200

    init styles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+  # TODO: set fly.io https://www.youtube.com/watch?v=E6UCy20ZKtA&ab_channel=Deanin
+  # Init styles.
+  
   def home
     @home = PagesContent.first
     @title = @home.try(:home_title)
