850875a2357cf253959f6047e8bb1fb85510e32a
Javier Cuadrado <mantecajc@gmail.com>
2023-07-01 21:55:07 +0200

    wip ok

--- app/javascript/controllers/turbo_modal_controller.js
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -6,4 +6,10 @@ export default class extends Controller {
     this.element.parentElement.removeAttribute("src") // it might be nice to also remove the modal SRC
     this.element.remove()
   }
+
+  submitEnd(e) {
+    if (e.detail.success) {
+      this.hideModal()
+    }
+  }
 }
