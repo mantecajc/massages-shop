2ca3edcdafca229a3c67b23ce7f4ced57d87ff8e
Javier Cuadrado <mantecajc@gmail.com>
2023-09-23 07:20:02 +0200

    wip mailjet

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -2,16 +2,14 @@ class InfoController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index]
 
   def index
-    # address = "9 Rue du Tabuteau, 49370 Saint-Clément-de-la-Place"
-    # coordinates = Geocoder.coordinates(address)
-    # if coordinates.present?
-    # lat = coordinates[0]
-    # lng = coordinates[1]
     @marker = [{
       lat: 47.5245343,
       lng: -0.7441239,
       info_window_html: render_to_string(partial: 'info_window')
     }]
-    # end
+  end
+
+  def contact
+    UserMailer.contact_email(@user).deliver_now
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,16 @@ module LESMassagesDePauline
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # config.action_mailer.delivery_method = :mailjet
+    # config.action_mailer.mailjet_settings = {
+    #   api_key: ENV['MAILJET_API_KEY'],
+    #   secret_key: ENV['MAILJET_SECRET_KEY']
+    # }
+    # TODO: Add domain name to mailjet
+    # TODO: Create alias sender address -> https://support.starred.com/docs/adding-alias-sender-addresses
+    # TODO: Add below
+    # config.default_from = 'example@here.com'
+    # TODO: Change owner
   end
 end
