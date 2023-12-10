e77050d9b5841d202c058c05d26c1db7f43349f7
Javier Cuadrado <mantecajc@gmail.com>
2023-12-10 10:38:23 +0100

    fix: fix bug on new massage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def massages_page?
-    (params[:filter] == 'massages') || (@massage && @massage.category.page == 'massages')
+    (params[:filter] == 'massages') || (@massage && @massage.category && @massage.category.page == 'massages')
   end
 
   def offers_page?
-    (params[:filter] == 'offers') || (@massage && @massage.category.page == 'offers')
+    (params[:filter] == 'offers') || (@massage && @massage.category && @massage.category.page == 'offers')
   end
 end
