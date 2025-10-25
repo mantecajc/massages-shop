9413699d66d8e462c74d44aa87c1b26feccdde3f
Javier Cuadrado <mantecajc@gmail.com>
2025-10-25 06:44:30 +0200

    Add send_email_service.rb

---
+++ app/services/send_email_service.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+
+class SendEmailService
+  attr_reader :to, :html_content, :subject
+
+  def initialize(to:, html_content:, subject:)
+    @to = to
+    @html_content = html_content
+    @subject = subject
+  end
+
+  def call
+    Mailjet::Send.create(
+      messages: [{
+        'From'=> {
+          'Email'=> sender,
+          'Name'=> subject
+        },
+        'To'=> [{
+          'Email'=> to,
+          # 'Name'=> params[:name] # Note(mantecajc): value not printed in the email
+        }],
+        'Subject'=> subject,
+        # 'TextPart'=> params[:message], # Note(mantecajc): value not printed in the email when sending HTMLPart
+        'HTMLPart'=> html_content
+      }]
+    )
+  end
+
+  private
+
+  def sender
+    ENV['MAILJET_SENDER']
+  end
+end
